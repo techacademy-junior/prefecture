afc7ee53f83f8199798967822dfce44c5df2d7d8
tomoaki-kimura <tkim19750404@gmail.com>
2019-11-12 07:28:20 +0900

    fix kyoto_bug

--- app.rb
+++ app.rb
@@ -9,7 +9,11 @@ get '/' do
 	  c = keyword.index("市") || 0
 	  @prefecture = ""#ここにコードを書いてみよう
 		@prefecture = "" if @prefecture.size == 1
-	  @city = ""#ここにコードを書いてみよう
-  end
-  erb :index
+		@city = ""#ここにコードを書いてみよう
+		if @prefecture == "京都"
+			@prefecture = "京都府"
+			@city = keyword[@prefecture.size, c - b]
+		end
+	end
+	erb :index
 end
