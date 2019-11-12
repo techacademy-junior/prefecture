6d5111ae7cea65a0636667a0f8880f87450b0348
tomoaki-kimura <tkim19750404@gmail.com>
2019-11-12 14:00:50 +0900

    modyfy app.rb delete variable"

--- app.rb
+++ app.rb
@@ -3,17 +3,16 @@ require 'sinatra'
 get '/' do
 
   if !params[:keyword].nil? && !params[:keyword].empty?
-	  keyword = params[:keyword]
-	  a = keyword.length
-	  b = keyword.index(/都|道|府|県/) || 0
-	  c = keyword.index("市") || 0
-	  @prefecture = ""#ここにコードを書いてみよう
-		@prefecture = "" if @prefecture.size == 1
-		@city = ""#ここにコードを書いてみよう
-		if @prefecture == "京都"
-			@prefecture = "京都府"
-			@city = keyword[@prefecture.size, c - b]
-		end
-	end
-	erb :index
+    keyword = params[:keyword]
+    a = keyword.index(/都|道|府|県/) || 0
+    b = keyword.index("市") || 0
+    @prefecture = ""#ここにコードを書いてみよう
+    @prefecture = "" if @prefecture.size == 1
+    @city = ""#ここにコードを書いてみよう
+    if @prefecture == "京都"
+      @prefecture = "京都府"
+      @city = keyword[@prefecture.size, b - a]
+    end
+  end
+  erb :index
 end
